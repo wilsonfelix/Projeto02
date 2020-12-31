2428bb90112e6386bd689b12aac5582d02e14ea6
wilsonfelix <wilson.felix.jr@gmail.com>
2020-12-31 01:27:46 -0300

    02

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using Projeto02.Entities; //importando
-using Projeto02.Repositories; //importando
-using System.IO;
+﻿using Projeto02.Entities; //importando
 using Projeto02.Inputs;
+using Projeto02.Repositories; //importando
+using System;
+using System.IO;
 
 
 
@@ -13,18 +13,18 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("\n *** EXPORTADOR DE PRODUTOS *** \n");
-            
+
             //tratamento de exceções
             try //tentativa!
             {
                 //criar um objeto para a classe produto...
                 //ler os dados do produto informado pelo usuário...
                 var produtoRepositoryGeradorArq = new ProdutoRepositoryGeradorArq();
-                
+
                 var menu = new Menu();
                 var produto = new Produto();
                 produto.IdProduto = Guid.NewGuid();
-                
+
                 //********************************************
                 //Recebe os metodos de entrada de dados da classe "ProdutoInput" para receber os dados
                 //inseridos pelo usuário
@@ -37,24 +37,24 @@ namespace ConsoleApp1
                 //exportar os dados do produto para arquivo...
                 var menuJSON_XML = new MenuJSON_XML();
                 menu.seletor = menuJSON_XML.SelecionarArquivo();
-                
+
                 if (menu.seletor == 1)
                 {
                     produtoRepositoryGeradorArq.GeraJSON();
-                
+
                 }
-                else if(menu.seletor == 2)
+                else if (menu.seletor == 2)
                 {
                     produtoRepositoryGeradorArq.GeraXML();
 
                 }
-                else if(menu.seletor ==3)
+                else if (menu.seletor == 3)
                 {
                     produtoRepositoryGeradorArq.GeraAmbos();
 
                 }
             }
-            catch(Exception e) //captura do erro!
+            catch (Exception e) //captura do erro!
             {
                 //imprimir mensagem de erro
                 Console.WriteLine("\nOcorreu um erro: " + e.Message + "\nO log do erro pode ser lido no caminho: " + @"h:\Projetos\Repositorio_local\Projeto02\");
